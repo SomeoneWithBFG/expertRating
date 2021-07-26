77f5990a14cddd3f3005323be61e81622a7f3291
SomeoneWithBFG <klastorka281000@gmail.com>
2021-07-26 15:47:07 +0300

    add whoAmI endpoint

--- core/src/controllers/auth/interfaces.ts
+++ core/src/controllers/auth/interfaces.ts
@@ -3,4 +3,5 @@ import { Request, Response } from 'express'
 export interface IAuthController {
     login: (req: Request, res: Response) => void
     refreshToken: (req: Request, res: Response) => void
+    whoAmI: (req: Request, res: Response) => void
 }
